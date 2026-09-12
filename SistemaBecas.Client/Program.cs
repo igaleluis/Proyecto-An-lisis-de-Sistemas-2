12777885fe5490551ad2a857474d7153180216fa
jefer <jefer@JEFERSON>
2026-09-12 13:42:48 -0600

    se agregaron las dos paginas de jeferson

--- SistemaBecas.Client/Program.cs
+++ SistemaBecas.Client/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
